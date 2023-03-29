68dde44f1733aee6864f197330c421418b32a4b0
Turner Allen <turneradev@gmail.com>
2023-03-29 00:08:08 -0700

    Update Premake5.lua

--- Premake5.lua
+++ Premake5.lua
@@ -14,17 +14,9 @@ project "GLFW"
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
-		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c",
+		"src/window.c"
 	}
-
 	filter "system:linux"
 		pic "On"
 
