8309df9d15597594d8b7e23ef3a1e22c9775305f
Turner Allen <turneradev@gmail.com>
2023-03-28 23:40:48 -0700

    Update Premake5.lua

--- Premake5.lua
+++ Premake5.lua
@@ -4,31 +4,27 @@ project "GLFW"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
 	files
 	{
-	    "include/GLFW/glfw3.h",
-	    "include/GLFW/glfw3native.h",
-	    "src/internal.h",
-	    "src/platform.h",
-	    "src/mappings.h",
-	    "src/context.c",
-	    "src/init.c",
-	    "src/input.c",
-	    "src/monitor.c",
-	    "src/platform.c",
-	    "src/vulkan.c",
-	    "src/window.c",
-	    "src/egl_context.c",
-	    "src/osmesa_context.c",
-	    "src/null_platform.h",
-	    "src/null_joystick.h",
-	    "src/null_init.c",
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
 
-	    "src/null_monitor.c",
-	    "src/null_window.c",
-	    "src/null_joystick.c",
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
 
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
 	}
+
 	filter "system:linux"
 		pic "On"
 
